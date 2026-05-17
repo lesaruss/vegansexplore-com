6ae85fe6054e375e0e77e8fa506548f33d97065b
Sean A. Russell <contact@lesaruss.com>
2026-05-17 12:14:01 -0400

    fix: activate Communities nav link

--- public/nav.js
+++ public/nav.js
@@ -76,7 +76,7 @@
           '</div>' +
         '</div>' +
         '<a href="/guides/ve-discuss"' + (isCurrent('/guides/ve-discuss') || isCurrent('/guides/ve-guide-chat-v1') ? ' aria-current="page"' : '') + '>Discuss</a>' +
-        '<a href="#" >Communities</a>' +
+        '<a href="/communities"' + cur('/communities') + '>Communities</a>' +
         '<a href="/directory"' + cur('/directory') + '>Directory</a>' +
         '<a href="/passport"' + cur('/passport') + '>Passport</a>' +
       '</div>' +
