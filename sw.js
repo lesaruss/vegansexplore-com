d3c6618e2be82d079c60e12a7bf9ef3bbfc18bd4
lesaruss <contact@lesaruss.com>
2026-05-30 11:02:25 -0400

    Add PWA service worker

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'shell-v1';
+const SHELL = ['/', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL).catch(()=>{})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      fetch(e.request).catch(() => caches.match('/index.html'))
+    );
+  }
+});
