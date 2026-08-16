f40ce92fc8001ecce776a81f33bc6ce1f3c6209a
lesaruss <contact@lesaruss.com>
2026-08-16 16:41:43 -0700

    Add Podcasts link to shared nav shell mobile menu

--- public/nav.js
+++ public/nav.js
@@ -213,6 +213,7 @@
         '<a href="' + welcomeHref + '"' + cur(welcomeHref) + '>Welcome</a>' +
         '<a href="/guides"' + cur('/guides') + '>Guides</a>' +
         '<a href="/pulse"' + (isCurrent('/pulse') || isCurrent('/guides/ve-discuss') ? ' aria-current="page"' : '') + '>Pulse</a>' +
+        '<a href="/podcast"' + (isCurrent('/podcast') || p.indexOf('/podcast/') === 0 ? ' aria-current="page"' : '') + '>Podcasts</a>' +
         '<a href="/communities"' + cur('/communities') + '>Communities</a>' +
         '<a href="/lead"' + cur('/lead') + '>Lead Your City</a>' +
         (isRealSuperAdmin ? '<a href="/directory"' + cur('/directory') + '>Directory</a>' : '') +
